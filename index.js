b0a9da189bc80fc5d482f5d99cd884fe3154edfd
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-12-20 14:46:10 +0300

    last touches

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ divs.forEach(item => {
     item.querySelector("p").textContent.toLowerCase().includes(word) ? (item.style.display = "block") : (item.style.display = "none");
 })
 })
+
+gsap.from('h1', { duration: 2, rotation: 720, delay: 0.5, x: '-100vw', ease: 'power2'});
+gsap.from('footer', { duration: 2, delay: 0.5, x: '-100vw', ease: 'power2.in'});
